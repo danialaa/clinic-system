dcfe9219baf930f86a559e6b1769afbc2d652954
Mostafa Amgad <mosta.king1@gmail.com>
2019-12-04 23:50:03 +0200

    equipment controller

--- ClinicSystem/src/com/example/clinicsystem/controllers/C_Equipment.java
+++ ClinicSystem/src/com/example/clinicsystem/controllers/C_Equipment.java
@@ -1,18 +1,40 @@
 package com.example.clinicsystem.controllers;
 
 import com.example.clinicsystem.helpers.TextFieldValidator;
-import com.example.clinicsystem.models.classes.M_Equipment;
+import com.example.clinicsystem.models.classes.*;
+import com.example.clinicsystem.models.enums.EquipmentType;
 
 import javax.swing.*;
 import java.util.List;
 
 public class C_Equipment {
-    public List request(String requestType, List list) {
+    public List request(String requestType, List data) {
         M_Equipment equipment = new M_Equipment();
 
         switch (requestType) {
             case "C":
-                equipment.addEquipment();
+                equipment.setName(data.get(0).toString());
+                equipment.setPrice(Integer.parseInt(data.get(1).toString()));
+                equipment.setQuantity(Integer.parseInt(data.get(2).toString()));
+                equipment.setEquipmentType(EquipmentType.getConstant(data.get(3).toString()));
+                equipment.setAvailable((boolean)data.get(4));
+                equipment.setId(equipment.addEquipment());
+
+                if(data.get(5).toString().equals("drug")) {
+                    M_Drug drug = new M_Drug();
+                    drug.setExpiryDate(data.get(6).toString());
+                    drug.setProductionDate(data.get(7).toString());
+                    drug.addDrug();
+                }
+                if(data.get(5).toString().equals("engine")) {
+                    M_DentalEngine dentalEngine = new M_DentalEngine();
+                }
+                if(data.get(5).toString().equals("operational")) {
+                    M_OperationalTool operationalTool = new M_OperationalTool();
+                }
+                if(data.get(5).toString().equals("sterilization")) {
+                    M_SterilizationTool sterilizationTool = new M_SterilizationTool();
+                }
 
                 break;
             case "R":
